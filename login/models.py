addd685dbdcd1b712c9f2908f794e038ab66043a
Victor Rodrigues Torres <torres14victor@hotmail.com>
2022-10-25 21:01:13 -0300

    testeee

--- login/models.py
+++ login/models.py
@@ -12,11 +12,42 @@ class User(models.Model):
     cash = models.DecimalField(max_digits=12, decimal_places=2, default=1000000)
     init = models.DecimalField(max_digits=12, decimal_places=2, default=1000000)
     createdAt = models.DateTimeField(auto_now_add=True)
-    tipoUser = models.CharField(blank=False, null=False, max_length=256, default="ALUNO")
 
     def __str__(self):
         return 'User' + self.username
 
     class Meta:
         verbose_name_plural = 'Users'
+
+
+class Professor(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=128)
+    email = models.EmailField(unique=True)
+    password = models.CharField(blank=False, null=False, max_length=256)
+    cash = models.DecimalField(max_digits=12, decimal_places=2, default=1000000)
+    init = models.DecimalField(max_digits=12, decimal_places=2, default=1000000)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'User' + self.username
+
+    class Meta:
+        verbose_name_plural = 'Professores'
+
+
+class Aluno(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=128)
+    email = models.EmailField(unique=True)
+    password = models.CharField(blank=False, null=False, max_length=256)
+    cash = models.DecimalField(max_digits=12, decimal_places=2, default=1000000)
+    init = models.DecimalField(max_digits=12, decimal_places=2, default=1000000)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'User' + self.username
+
+    class Meta:
+        verbose_name_plural = 'Alunos'
 
